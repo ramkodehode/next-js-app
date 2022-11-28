d74092d393b43675113282ac9949411391d41a74
ramkodehode <ram.kodehode@gmail.com>
2022-11-28 09:31:36 +0100

    Added extra text for the about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const About = () => {
-  return <div>THIS IS THE ABOUT PAGE</div>;
+  return <div>THIS IS THE ABOUT PAGE and Im am testing here</div>;
 };
 
 export default About;
